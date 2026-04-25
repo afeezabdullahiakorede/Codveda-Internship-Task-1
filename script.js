63ddda45bfe4e76ebd52aff1917047665da6011f
Abdullahi Akorede <aabdullahiakorede@gmail.com>
2026-04-25 11:51:10 -0700

    feat: styling, implement responsive grid and smooth scrolling, simple fade in animation

---
+++ script.js
@@ -0,0 +1,10 @@
+// Select all links with hashes
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
